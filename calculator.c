a8dfcf6d2e226f7388d35d978fceff646548557a
K-21-code <kashishkapoor1921@gmail.com>
2026-07-16 10:24:03 +0530

    Modified using while loop and conditionals

--- calculator.c
+++ calculator.c
@@ -1,31 +1,37 @@
-#include<stdio.h> 
-int main(){ 
-    int a,b;
-    char op;    
-    printf("Simple Calculator Starting...\nLoading modules.................\nREADY.\n"); 
-    printf("Enter two numbers: ");
-    scanf("%d %d",&a,&b);
-    printf("Enter operator (+, -, *, /): ");
-    scanf(" %c",&op);
-    printf("You entered: %d and %d\n", a, b);
-   if(op == '+'){
-    printf("SUM: %d", a + b);
-   }
-   else if(op == '-'){
-    printf("DIFFERENCE: %d", a - b);
-   }
-   else if(op == '*'){
-    printf("PRODUCT %d", a * b);
-   }
-   else if(op == '/'){
-    if(b != 0){printf("QUOTIENT: %d", a / b);
-    }
-    else{
-        printf("Error: Division by zero is not allowed.");
-    }
-   }
-    else{
-     printf("Invalid operator entered.");
-    }
-    return 0;
+#include <stdio.h>
+int main(){
+    int n1, n2;
+    char op;
+    printf("SIMPLE CALCULATOR\n");
+    while(1){
+        printf("Enter operation to perform(+, -, *, /)");
+        scanf(" %c",&op);
+        if(op == 'q' || op == 'Q'){
+            break; 
+        }
+        printf("Enter two numbers: \n");
+        scanf("%d %d",&n1,&n2);
+        if(op == '+'){
+            printf("SUM = %d\n", n1 + n2);
+        }
+        else if(op == '-'){
+            printf("DIFFERENCE = %d\n", n1 - n2);
+        }
+        else if(op == '*'){
+            printf("PRODUCT = %d\n", n1 * n2);
+        }
+        else if(op == '/'){
+            if(n2 != 0){
+            printf("QUOTIENT = %d\n", n1 / n2);
+            }
+            else{
+                printf("Division by zero not possible\n");
+            }
+        }
+        else{
+            printf("Invalid operation\n");
+        }
+        printf("Press 'q' to quit or any other key to continue\n");
+}
+return 0;
 }
