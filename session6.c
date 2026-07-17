ac1cce8e12980b5b04e6268c9311d6f96909b624
K-21-code <kashishkapoor1921@gmail.com>
2026-07-17 09:41:53 +0530

    Getting started with Functions in C

---
+++ session6.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int square(int n){
+    return n * n;
+}
+int main(){
+    int n;
+    printf("Enter number: ");
+    scanf("%d",&n);
+    printf("Square of %d: %d\n",n, square(n));
+    return 0;
+}
