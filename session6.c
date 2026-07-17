175d5cabc881241fa0ff813051ee93504d75fe80
K-21-code <kashishkapoor1921@gmail.com>
2026-07-17 09:52:21 +0530

    Getting started with functions in C

--- session6.c
+++ session6.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-int square(int n){
-    return n * n;
-}
-int main(){
-    int n;
-    printf("Enter number: ");
-    scanf("%d",&n);
-    printf("Square of %d: %d\n",n, square(n));
+int max(int a, int b){
+    if(a > b){
+        return a;
+    }else{
+        return b;
+    }
+}int main(){
+    int n1, n2;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&n1,&n2);
+    printf("%d is the larger number\n", max(n1,n2));
     return 0;
 }
