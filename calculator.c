e606610b9f35acb16569dbf37e5f3a9df51a5bad
K-21-code <kashishkapoor1921@gmail.com>
2026-07-21 09:05:22 +0530

    modified calculator using pass by reference

--- calculator.c
+++ calculator.c
@@ -1,16 +1,24 @@
 #include <stdio.h>
-int add(int n1, int n2){
-    return n1+n2;
-}
-int subtract(int n1, int n2){
-    return n1-n2;
-}
-int multiply(int n1, int n2){
-    return n1*n2;
-}
-int divide(int n1, int n2){
-    return n1/n2;
-}
+int calculate(char op, int a, int b){
+ if(op == '+'){ 
+    return a + b;
+        }
+        else if(op == '-'){
+            return a - b;
+         }
+        else if(op == '*'){
+            return a * b;
+          }
+        else if(op == '/'){
+            if(b == 0){
+                printf("Error: Division by zero not possible\n");
+            } else {
+            return a / b;
+         }
+    }else{
+        printf("INVALID OPERATION\n");
+    }printf("Press 'q' to quit or any other key to continue\n");
+    }
 int main(){
     int n1,n2;
     char op;
@@ -22,23 +30,8 @@ int main(){
         }
         printf("Enter two numbers: ");
         scanf("%d %d",&n1,&n2);
-        if(op == '+'){
-            printf("SUM = %d\n", add(n1,n2));
-        }
-        else if(op == '-'){
-            printf("DIFFERENCE = %d\n", subtract(n1,n2));
-        }
-        else if(op == '*'){
-            printf("PRODUCT = %d\n", multiply(n1,n2));
-        }
-        else if(op == '/'){
-            if(n2 == 0){
-                printf("Error: Division by zero not possible\n");
-            } else {
-            printf("QUOTIENT = %d\n", divide(n1,n2));
-        }
-    }else{
-        printf("INVALID OPERATION\n");
-    }printf("Press 'q' to quit or any other key to continue\n");
-    }return 0;
+        int result = calculate(op, n1, n2);
+        printf("RESULT = %d\n", result);
+    }
+    return 0;
 }
