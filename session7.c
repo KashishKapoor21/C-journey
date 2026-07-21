5c346bec9f389d1a64e0bb0cc13a877848d13855
K-21-code <kashishkapoor1921@gmail.com>
2026-07-21 08:11:04 +0530

    pass by refernce

--- session7.c
+++ session7.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-void swap(int a, int b){
-    int c = a;
-    a = b;
-    b = c;
-    printf("%d %d", a, b);
+void swap(int *a, int *b){
+    int c = *a;
+    *a = *b;
+    *b = c;
 }
 int main(){
-    swap(20, 49);
+    int x = 20, y = 34;
+    swap(&x, &y);
+    printf("%d %d", x, y);
 }
